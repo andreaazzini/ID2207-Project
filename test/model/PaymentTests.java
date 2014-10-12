d5f7779069be74ef895e263486c45d3c78d4f4c8
andreaazzini <andrea2.azzini@mail.polimi.it>
2014-10-12 15:55:31 +0200

    PaymentTests implementation

---
+++ test/model/PaymentTests.java
@@ -0,0 +1,47 @@
+package model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PaymentTests {
+	private Payment payment;
+	private Client client;
+	private int amount;
+	
+	@Before
+	public void setUp() {
+		amount = 5000;
+		client = new Client("jan", "tomljanovic", 22, "jt@email.com", "07365873937", "KDJF3487KD", null);
+		payment = new Payment(client, amount);
+	}
+	
+	@Test
+	public void getIbanTest() {
+		Assert.assertEquals(payment.getIBAN(), client.getIBAN());
+	}
+	
+	@Test
+	public void setIbanTest() {
+		String iban = "LSDUGH2183";
+		payment.setIBAN(iban);
+		Assert.assertEquals(payment.getIBAN(), iban);
+	}
+	
+	@Test
+	public void getClientTest() {
+		Assert.assertEquals(payment.getClient(), client);
+	}
+	
+	@Test
+	public void getAmountTest() {
+		Assert.assertEquals(payment.getAmount(), amount);
+	}
+	
+	@Test
+	public void isApprovedTest() {
+		Assert.assertFalse(payment.isApproved());
+		payment.approve();
+		Assert.assertTrue(payment.isApproved());
+	}
+}
